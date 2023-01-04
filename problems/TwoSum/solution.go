9e605e2c6af9c946520b2b8c0b1d70b81698521c
David Lombera <david.lomberlan@gmail.com>
2023-01-04 00:52:57 -0600

    Add solutions for "1. Two Sum" problem

---
+++ problems/TwoSum/solution.go
@@ -0,0 +1,14 @@
+package twosum
+
+func twoSum(nums []int, target int) []int {
+	rest := make(map[int]int, 0)
+
+	for i, num := range nums {
+		if j, exists := rest[target-num]; exists {
+			return []int{j, i}
+		}
+		rest[num] = i
+	}
+
+	return nil
+}
