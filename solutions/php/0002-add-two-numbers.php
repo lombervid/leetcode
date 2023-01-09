c277e9a4c6f3c51025efe7b5e0e30be6f933fe5a
David Lombera <david.lomberlan@gmail.com>
2023-01-08 23:33:29 -0600

    Refactor php solution for 2. Add Two Numbers

--- solutions/php/0002-add-two-numbers.php
+++ solutions/php/0002-add-two-numbers.php
@@ -17,35 +17,22 @@ class Solution
      * @param ListNode $l2
      * @return ListNode
      */
-    function addTwoNumbers($l1, $l2)
+    function addTwoNumbers(ListNode $l1, ListNode $l2): ListNode
     {
         $list = new ListNode();
         $last = $list;
         $carry = 0;
 
         while ($l1 || $l2 || $carry) {
-            $num1 = 0;
-            $num2 = 0;
-
-            if ($l1) {
-                $num1 = $l1->val;
-                $l1 = $l1->next;
-            }
-
-            if ($l2) {
-                $num2 = $l2->val;
-                $l2 = $l2->next;
-            }
-
-            if (($sum = $num1 + $num2 + $carry) >= 10) {
-                $sum -= 10;
-                $carry = 1;
-            } else {
-                $carry = 0;
-            }
+            $sum = ($l1 ? $l1->val : 0) + ($l2 ? $l2->val : 0) + $carry;
+            $carry = floor($sum / 10);
+            $sum %= 10;
 
             $last->next = new ListNode($sum);
             $last = $last->next;
+
+            $l1 = $l1?->next;
+            $l2 = $l2?->next;
         }
 
         return $list->next;
