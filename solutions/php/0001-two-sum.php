6c591bdf71b94fd5fdb8357608e9257dfc592866
David Lombera <david.lomberlan@gmail.com>
2023-01-08 19:06:48 -0600

    Add php solution for 1. Two Sum

---
+++ solutions/php/0001-two-sum.php
@@ -0,0 +1,21 @@
+<?php
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target)
+    {
+        $rest = [];
+
+        foreach ($nums as $i => $num) {
+            if (array_key_exists($target - $num, $rest)) {
+                return [$i, $rest[$target - $num]];
+            }
+
+            $rest[$num] = $i;
+        }
+    }
+}
