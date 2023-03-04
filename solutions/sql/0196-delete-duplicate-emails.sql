d4d7ecefb9cc453c45db2972b6b2aaba4e01b871
David Lombera <david.lomberlan@gmail.com>
2023-03-04 13:36:17 -0600

    Add 196. Delete Duplicate Emails

---
+++ solutions/sql/0196-delete-duplicate-emails.sql
@@ -0,0 +1,11 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+DELETE
+FROM Person
+WHERE id NOT IN (
+    SELECT id FROM(
+        SELECT MIN(id) as id
+        FROM Person
+        GROUP BY email
+    ) AS duplicates
+);
